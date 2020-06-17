d3979253b371fbf902be57393cde94df7bdd197e
skachkova-natalia <natalia-skachkova@yandex.ru>
2020-06-17 18:30:20 +0300

    feat: sprint-game html

---
+++ src/js/sprint/index.js
@@ -0,0 +1,8 @@
+class SprintGame {
+  constructor() {
+    this.timeDuration = document.querySelector('.sprint #time-duration').value;
+    this.difficulty = document.querySelector('.sprint #difficulty').value;
+  }
+}
+
+export default SprintGame;
