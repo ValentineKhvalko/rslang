b8b5fbc9ec630dcda23b8f089333b0ec8abf4d01
bobro <bobrow.bobro2015@yandex.ru>
2020-06-21 16:52:44 +0300

    feat: add equal new words and cards

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ document.querySelector('.settings-img').src = require('./components/img/settings
 document.querySelector('.games-close-img').src = require('./components/img/close.png').default;
 document.querySelector('.settings-close-img').src = require('./components/img/close.png').default;
 
+document.querySelector('#new-words').value = '10';
+document.querySelector('#cards-words').value = '10';
+
 document.querySelector('.settings').addEventListener('click', () => {
 	document.querySelector('.st').classList.toggle('hidden');
 	document.querySelector('#translation').checked = 'checked';
-	document.querySelector('#new-words').value = '';
-	document.querySelector('#cards-words').value = '';
 	document.querySelector('.gm').classList.add('hidden');
 })
 
@@ -29,4 +30,30 @@ document.querySelector('.settings-close-img').addEventListener('click', () => {
 
 document.querySelector('.games').addEventListener('click', () => {
 	document.querySelector('.gm').classList.toggle('hidden');
+	document.querySelector('.st').classList.add('hidden');
 })
+
+function equal() {
+	let a = +document.querySelector('#new-words').value;
+	if(document.querySelector('#new-words').value.trim() === '')
+		a = 0;
+	let b = +document.querySelector('#cards-words').value
+	if(document.querySelector('#cards-words').value.trim() === '')
+		b = 0;
+	if(a > b)
+		a = b;
+	document.querySelector('#new-words').value = a;
+	document.querySelector('#cards-words').value = b;
+}
+
+document.querySelector('#new-words').addEventListener('change', equal);
+document.querySelector('#cards-words').addEventListener('change', equal);
+
+
+/*const getWords = async (page, group) => {
+  const url = `https://afternoon-falls-25894.herokuapp.com/words?page=${page}&group=${group}`;
+  const res = await fetch(url);
+  const obj = await res.json();
+  let words = +document.querySelector('#new-words').value;
+  let cards = +document.querySelector('#cards-words').value;
+};*/
