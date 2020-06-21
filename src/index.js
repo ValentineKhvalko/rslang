ed95568747ca05eadac69df77a2ec1cf473a617f
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-06-21 02:50:19 -0400

    feat: add start page layout and styles

---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './js/audio.js';
---
+++ src/js/audio.js
@@ -0,0 +1,7 @@
+import '../components/styles/audio.scss';
+
+// function Redirect() {
+//   window.location.href = '/audio.html';
+// }
+
+// document.getElementById('a-button').addEventListener('click', Redirect);
