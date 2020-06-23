36e5280c96068dbc99dfa6f3f478993243979534
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-06-23 03:06:18 -0400

    feat: add settings window, change styles

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,5 +1,14 @@
 import '../components/styles/audio.scss';
 
+document.querySelector('.a_settings-link').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.a_settings').classList.toggle('hidden');
+});
+
+document.querySelector('.a_close-button').addEventListener('click', () => {
+  document.querySelector('.a_settings').classList.toggle('hidden');
+});
+
 // function Redirect() {
 //   window.location.href = '/audio.html';
 // }
