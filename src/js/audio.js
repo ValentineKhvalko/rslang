abaae81b2418fccb46403ea2520a349c8bbe0b5a
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-06-25 15:48:47 -0400

    feat: add game page

--- src/js/audio.js
+++ src/js/audio.js
@@ -9,6 +9,11 @@ document.querySelector('.a_close-button').addEventListener('click', () => {
   document.querySelector('.a_settings').classList.toggle('hidden');
 });
 
+document.querySelector('.a_start-button').addEventListener('click', () => {
+  document.querySelector('.a_start').classList.toggle('hidden');
+  document.querySelector('.a_game').classList.toggle('hidden');
+});
+
 // function Redirect() {
 //   window.location.href = '/audio.html';
 // }
