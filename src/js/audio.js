c4da90db8c65623eb580458ebc5492e7a50469e8
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-06-26 13:36:32 -0400

    feat: add round choice to settings, settings link to game page

--- src/js/audio.js
+++ src/js/audio.js
@@ -14,6 +14,12 @@ document.querySelector('.a_start-button').addEventListener('click', () => {
   document.querySelector('.a_game').classList.toggle('hidden');
 });
 
+// TO DO  FIX - settings not visible
+document.querySelector('.a_game-settings-link').addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('.a_settings').classList.toggle('hidden');
+});
+
 // function Redirect() {
 //   window.location.href = '/audio.html';
 // }
