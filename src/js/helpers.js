6d5dfa5c5816648b6056aeb1d5c17a5a635319a3
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-06-30 15:09:31 -0400

    feat: add function to find object by key value, change logic to play audio

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,6 @@
 export function playAudio() {
-  const sourceNumber = selectRandomNumber(JSON.parse(localStorage.getItem('a_mediaData')));
+  const currentWord = JSON.parse(localStorage.getItem('a_currentWord'));
+  const sourceNumber = currentWord.mediaNumber;
   const audio = new Audio();
   let str = '';
   str = `https://raw.githubusercontent.com/tanya-kh/rslang-data/master/files/${sourceNumber}.mp3`;
@@ -7,8 +8,17 @@ export function playAudio() {
   audio.play();
 }
 
-function selectRandomNumber(array) {
+export function selectRandomNumber(array) {
   const randomItem = array[Math.floor(Math.random() * array.length)];
   //   console.log(randomItem);
   return randomItem;
+}
+
+export function findObjectByKey(array, key, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][key] === value) {
+      return array[i];
+    }
+  }
+  return null;
 }
