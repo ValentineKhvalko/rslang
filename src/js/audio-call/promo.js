bfb9229198298918ff5e5734681131341b72f84b
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-07-09 23:57:09 -0400

    refactor: add promo page and styles

---
+++ src/js/audio-call/promo.js
@@ -0,0 +1,8 @@
+import '../../components/styles/promo.scss';
+
+document.getElementById('a_denis').src = require('../../components/a_assets/photoIcon.jpg').default;
+document.getElementById('a_natalia').src = require('../../components/a_assets/photoIcon.jpg').default;
+document.getElementById('a_alexandr').src = require('../../components/a_assets/photoIcon.jpg').default;
+document.getElementById('a_andrei').src = require('../../components/a_assets/photoIcon.jpg').default;
+document.getElementById('a_tanya').src = require('../../components/a_assets/tanya.jpg').default;
+document.getElementById('a_valentin').src = require('../../components/a_assets/valentin.jpg').default;
