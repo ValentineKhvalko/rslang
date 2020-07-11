35dadd593de259b0a8cd080908c1adfc66b55a74
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-07-11 12:26:13 -0400

    feat: add Natasha's photo, remove Andrei

--- src/js/audio-call/promo.js
+++ src/js/audio-call/promo.js
@@ -1,7 +1,7 @@
 import '../../components/styles/promo.scss';
 
 document.getElementById('a_denis').src = require('../../components/a_assets/photoIcon.jpg').default;
-document.getElementById('a_natalia').src = require('../../components/a_assets/photoIcon.jpg').default;
+document.getElementById('a_natalia').src = require('../../components/a_assets/natasha.jpg').default;
 document.getElementById('a_alexandr').src = require('../../components/a_assets/photoIcon.jpg').default;
 document.getElementById('a_andrei').src = require('../../components/a_assets/photoIcon.jpg').default;
 document.getElementById('a_tanya').src = require('../../components/a_assets/tanya.jpg').default;
